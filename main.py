537d5057c7d662f483646e35c3877bd2e4ad076d
ghufrankochi <ghufran.kochi@gmail.com>
2025-01-15 21:43:37 +0430

    main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("سلام! زه ستا بوټ یم.")
+    await update.message.reply_text("السلام علیکم! زه ستا بوټ یم.")
 
 if __name__ == "__main__":
     app = ApplicationBuilder().token("1095818301:AAEXh1vV3WsMf8Vxoy5aC6yslmIOGQ7_t_0").build()
